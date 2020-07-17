3f81d323cb827511a263cee3a7a000ba06e52491
Ezequiel <ezequiel3434@gmail.com>
2020-07-17 12:16:56 -0300

    Challange fineshed

--- Challange-Day23-UIKit/DetailViewController.swift
+++ Challange-Day23-UIKit/DetailViewController.swift
@@ -18,8 +18,16 @@ class DetailViewController: UIViewController {
         title = title2
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
+            imageView.layer.borderWidth = 2
+            imageView.layer.borderColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0).cgColor
+            imageView.layer.cornerRadius = 5.0
+
+            imageView.contentMode = .scaleAspectFit
+            
         }
         
     }
@@ -32,6 +40,15 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.pngData() else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        present(vc, animated: true)
     }
 
 
